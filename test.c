490370bcaea7804912bd12316c16fd19f5477600
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2017-03-23 12:12:42 +0000

    modification

--- test.c
+++ test.c
@@ -1,95 +1,27 @@
-
-//this program is for testing
-
-#include "wave.h"
 #include <stdio.h>
-#include <math.h>
+#include <curl/curl.h>
 
-//this program will open a wave file and dplay wav header siinfo
-int main(int argc, char*argv[])
+int main(void)
 {
-	FILE*fp;
-	WAVHDR myhdr;
-	int answer;
-	if(argc != 2)
+	CURL *curl;
+	CURLcode res;
+
+	curl_global_init(CURL_GLOBAL_ALL);	//globally initiate curl
+	curl = curl_easy_init();	//make a curl handler
+	if(curl)
 	{
-		printf("Usage : %s wave_file\n", argv[0]);
-		return -1;
+		//Set URL of server page
+		curl_easy_setopt(curl, CURLOPT_URL, "http://www.cc.puv.fi/~e1601128/curl.php");
+		//Set post data
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "value=100&from=e1601128");
+		//Perform a submit
+		res = curl_easy_perform(curl);
+		if(res != CURLE_OK)	//if the server side is negative
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+		}
+		curl_easy_cleanup(curl);
 	}
-	fp = fopen(argv[1], "r"); //try to open the wav file
-	if(fp == NULL)
-	{
-		printf("Cannot find the file\n", argv[1]);
-		return -1;
-	}
-	fread(&myhdr, sizeof(myhdr), 1, fp);
-	displayWAVHDR(myhdr);
-	fclose(fp);
-	printf("Do you want to generate a test tone? (1:yes, 0:no)\n");
-	scanf("%d", &answer);
-	if(answer == 1)	testTone(1000, 5);
-}
+	curl_global_cleanup();
+} 
 
-void testTone(int freq, double d)
-{
-	FILE*fp;
-	int i;
-	WAVHDR h;
-	short int sample;
-	fp = fopen("testtone.wav", "w");
-	fillID("RIFF", h.ChunkID);
-	//chunksizll be calculated later
-	fillID("WAVE", h.Format);
-	fillID("fmt ", h.Subchunk1ID);
-	h.Subchunk1Size = 16;
-	h.AudioFormat = 1;
-	h.SampleRate = SAMPLE_RATE;
-	h.BitsPerSample = 16;
-	h.ByteRate = SAMPLE_RATE*h.NumChannels*(h.BitsPerSample/8);
-	h.BlockAlign = h.NumChannels*(h.BitsPerSample/8);
-	fillID("data", h.Subchunk2ID);
-	h.Subchunk2Size = d*SAMPLE_RATE*(h.BitsPerSample/8);
-	fillID("data", h.Subchunk2ID);
-	h.Subchunk2Size = (int)h.ByteRate*d;
-	h.ChunkSize = h.Subchunk2Size + 36;
-	fwrite(&h, sizeof(h), 1, fp);
-	for(i=0; i<d*SAMPLE_RATE; i++)
-	{
-		sample = 32768*sin(2*PI*freq*i/SAMPLE_RATE);
-		fwrite(&sample, sizeof(sample), 1, fp);
-	}
-	fclose(fp); 
-}
-
-void displayWAVHDR(WAVHDR hdr)
-{
-	double duration;
-        printf("ChunkID: ");    printID(hdr.ChunkID);
-        printf("ChunkSize: %d\n", hdr.ChunkSize);
-        printf("Format: ");     printID(hdr.Format);
-        printf("Subchunk1 ID; ");       printID(hdr.Subchunk1ID);
-        printf("Subchunk1 Size: %d\n", hdr.Subchunk1Size);
-        printf("Audio Format: %d\n", hdr.AudioFormat);
-        printf("Number of Channel: %d\n", hdr.NumChannels);
-        printf("Sample Rate; %d\n", hdr.SampleRate);
-        printf("Byte Rate: %d\n", hdr.ByteRate);
-        printf("Block Align: %d\n", hdr.BlockAlign);
-        printf("Bits per Sample: %d\n", hdr.BitsPerSample);
-        printf("Subchunk2 ID; ");       printID(hdr.Subchunk2ID);
-        printf("Subchunk2 Size: %d\n", hdr.Subchunk2Size);
-	duration = (double)hdr.Subchunk2Size/hdr.ByteRate;
-	printf("Duration of Audio %.3f sec\n", duration);
-}
-
-void printID(char id[])
-{
-	int i;
-	for(i=0; i<4; i++) putchar(id[i]);
-	printf("\n");
-}
-
-void fillID(const char*s, char d[])
-{
-	int i;
-	for(i=0; i<4; i++)	d[i] = *s++;
-}
